690f5f510830e6ce6b2c4387a9a82318f31cd79e
Georgy Khaydenko <ghaidenko@gmail.com>
2020-06-25 14:21:28 +0300

    Initial Commit

---
+++ TestHotels/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestHotels
+//
+//  Created by Георгий Хайденко on 25.06.2020.
+//  Copyright © 2020 George Khaydenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
