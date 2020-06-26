53777e7813d189c8a6cfcb57af75335c950b2081
Georgy Khaydenko <ghaidenko@gmail.com>
2020-06-26 16:53:09 +0300

    refactored Hotel struct

--- TestHotels/Hotel.swift
+++ TestHotels/Hotel.swift
@@ -12,26 +12,26 @@ struct Hotel: Codable {
     let address: String
     let stars: Double
     let distance: Double
-    let suitesAvailability: [String]
+
+    var suitesAvailability: [String]
 
     enum CodingKeys: String, CodingKey {
         case id, name, address, stars, distance
         case suitesAvailability = "suites_availability"
     }
 
-    init(
-        id: Int,
-        name: String,
-        address: String,
-        stars: Double,
-        distance: Double,
-        suitesAvailability: String)
-    {
-        self.id = id
-        self.name = name
-        self.address = address
-        self.stars = stars
-        self.distance = distance
-        self.suitesAvailability = suitesAvailability.components(separatedBy: ":")
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        address = try container.decode(String.self, forKey: .address)
+        stars = try container.decode(Double.self, forKey: .stars)
+        distance = try container.decode(Double.self, forKey: .distance)
+
+        let suitesAvailabilityString = try container.decode(
+            String.self,
+            forKey: .suitesAvailability)
+
+        suitesAvailability = suitesAvailabilityString.split(separator: ":", omittingEmptySubsequences: true).map { String($0) }
     }
 }
