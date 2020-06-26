c9195801536ee390851ab4bd2a9c0f2a8e8c2a7d
Georgy Khaydenko <ghaidenko@gmail.com>
2020-06-26 13:29:31 +0300

    fix doubling issue due to different iOS versions

--- TestHotels/AppDelegate.swift
+++ TestHotels/AppDelegate.swift
@@ -15,10 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = MainViewController()
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
+        if #available(iOS 13, *) {
+        } else {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            let viewController = MainViewController()
+            window?.rootViewController = viewController
+            window?.makeKeyAndVisible()
+        }
 
         return true
     }
