68982f371a04c542535bafaefab1b9002ccd01cb
Georgy Khaydenko <ghaidenko@gmail.com>
2020-06-27 11:44:44 +0300

    add detail view

---
+++ TestHotels/Main/DetailView.swift
@@ -0,0 +1,30 @@
+//
+//  DetailView.swift
+//  TestHotels
+//
+//  Created by Георгий Хайденко on 27.06.2020.
+//  Copyright © 2020 George Khaydenko. All rights reserved.
+//
+
+import UIKit
+
+final class DetailView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        layoutContent(in: self)
+        applyStyle()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func layoutContent(in view: UIView) {
+        
+    }
+
+    private func applyStyle() {
+
+    }
+}
---
+++ TestHotels/Main/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  TestHotels
+//
+//  Created by Георгий Хайденко on 27.06.2020.
+//  Copyright © 2020 George Khaydenko. All rights reserved.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+    var detailView: DetailView!
+
+    override func loadView() {
+        detailView = DetailView()
+        view = detailView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
